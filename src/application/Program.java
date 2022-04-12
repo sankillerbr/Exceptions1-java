e9b20b3b241469929ea185b616904aead289c3aa
Sandro Marcio <sandromarcio@gmail.com>
2022-04-12 16:09:23 -0300

    Versão 1

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Room Number: ");
+		 
+	}
+
+}
